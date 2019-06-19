f604515529f43012fe67edda7b5930efa3670997
amelco <amelco.herman@gmail.com>
2019-06-19 11:54:49 -0300

    Define routes to / and /new

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';  // React em si
 import ReactDOM from 'react-dom';   // Integração do react em browsers
 import App from './App';
+import './global.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
